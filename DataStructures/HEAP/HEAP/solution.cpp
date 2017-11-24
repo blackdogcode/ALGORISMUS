f5247806f232e75c13f104328bf5283cfeb4e823
ANONYMOUS HOLY <deilttantest@gmail.com>
2017-11-24 23:36:10 +0900

    MIN & MAX HEAP

--- DataStructures/HEAP/HEAP/solution.cpp
+++ DataStructures/HEAP/HEAP/solution.cpp
@@ -1,3 +1,4 @@
+// https://algospot.com/judge/problem/read/RUNNINGMEDIAN
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <vector>
@@ -9,52 +10,43 @@ using namespace std;
 #define NEG_INF -123456789
 #define MAX_N 200000
 
-void swap(int &a, int &b);
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b; b = temp;
+}
 
 struct MinHeap {
-	MinHeap() : size(0) {
-		arr = new int[MAX_N + 10];
-	}
-	~MinHeap() {
-		delete[] arr;
-	}
+	MinHeap() : size(0) { arr = new int[MAX_N + 10]; }
+	~MinHeap() { delete[] arr; }
 
 	void insert(int newKey);
 	void pop();
 	int top();
 	bool isEmpty();
-	void display();
 
-	int size;
-	int *arr;
+	int *arr, size;
 };
 
 struct MaxHeap {
-	MaxHeap() : size(0) {
-		arr = new int[MAX_N + 10];
-	}
-	~MaxHeap() {
-		delete[] arr;
-	}
+	MaxHeap() : size(0) { arr = new int[MAX_N + 10]; }
+	~MaxHeap() { delete[] arr; }
 
 	void insert(int newKey);
 	void pop();
 	bool isEmpty();
 	int top();
-	void display();
 
-	int size;
-	int *arr;
+	int *arr, size;
 };
 
-int generateNewKey(int idx, int a, int b, int previous) {
+int generateNextKey(int idx, int a, int b, int prevKey) {
 	if (idx == 0) return 1983;
-	int next = (previous * (long long)a + b) % 20090711;
+	int next = (prevKey * (long long)a + b) % 20090711; // 오버 플로우 방지: long long
 	return next;
 }
 
 int main(int argc, char *argv[]) {
-	//freopen("sample_input.txt", "r", stdin);
+	freopen("sample_input.txt", "r", stdin);
 
 	int C; scanf("%d", &C);
 	for (int test_case = 1; test_case <= C; test_case++) {
@@ -64,46 +56,36 @@ int main(int argc, char *argv[]) {
 		MaxHeap leftMaxHeap;
 		MinHeap rightMinHeap;
 
-		int newKey = 0, result = 0;
-		for (int i = 0; i < N; i++) {
-			newKey = generateNewKey(i, a, b, newKey);
-			if (leftMaxHeap.size == rightMinHeap.size) {
-				if (leftMaxHeap.top() >= newKey)
-					leftMaxHeap.insert(newKey);
-				else {
-					rightMinHeap.insert(newKey);
-					int key = rightMinHeap.top();
-					rightMinHeap.pop();
-					leftMaxHeap.insert(key);
+		int nextKey = 0, result = 0;
+		for (int idx = 0; idx < N; idx++) {
+			nextKey = generateNextKey(idx, a, b, nextKey);
+			// 불변식 1: |leftMaxHeap| == |rightMinHeap| or |rightMinHeap| + 1
+			if (leftMaxHeap.size == rightMinHeap.size)
+				leftMaxHeap.insert(nextKey);
+			else
+				rightMinHeap.insert(nextKey);
+			// 불변식 2: leftMaxHeap.top <= rightMinHeap.top
+			if (!leftMaxHeap.isEmpty() && !rightMinHeap.isEmpty()) {
+				if (leftMaxHeap.top() > rightMinHeap.top()) {
+					int leftMax = leftMaxHeap.top();
+					int rightMin = rightMinHeap.top();
+					leftMaxHeap.pop(); rightMinHeap.pop();
+					leftMaxHeap.insert(rightMin);
+					rightMinHeap.insert(leftMax);
 				}
 			}
-			else {
-				if (leftMaxHeap.top() >= newKey) {
-					leftMaxHeap.insert(newKey);
-					int key = leftMaxHeap.top();
-					leftMaxHeap.pop();
-					rightMinHeap.insert(key);
-				}
-				else {
-					rightMinHeap.insert(newKey);
-				}
-			}
+			// 결과식: 불변식 1과 2만족 시, 중간 값은 항상 leftMaxHeap.top
 			int midKey = leftMaxHeap.top();
 			result = (result + midKey) % 20090711;
 		}
 		printf("%d\n", result);
 	}
-
 	return 0;
 }
 
-void swap(int &a, int &b) {
-	int temp = a;
-	a = b; b = temp;
-}
-
-
+//-----------------------------------------------//
 //---------------- Min Heap ---------------------//
+//-----------------------------------------------//
 void MinHeap::insert(int newKey) {
 	arr[++size] = newKey;
 
@@ -145,13 +127,10 @@ int MinHeap::top() {
 	if (isEmpty()) return POS_INF;
 	else return arr[1];
 }
-void MinHeap::display() {
-	for (int i = 1; i <= size; i++)
-		printf("%d ", arr[i]);
-	puts("");
-}
 
+//-----------------------------------------------//
 //---------------- Max Heap ---------------------//
+//-----------------------------------------------//
 void MaxHeap::insert(int newKey) {
 	arr[++size] = newKey;
 
@@ -192,9 +171,4 @@ bool MaxHeap::isEmpty() {
 int MaxHeap::top() {
 	if (isEmpty()) return NEG_INF;
 	else return arr[1];
-}
-void MaxHeap::display() {
-	for (int i = 1; i <= size; i++)
-		printf("%d ", arr[i]);
-	puts("");
 }
