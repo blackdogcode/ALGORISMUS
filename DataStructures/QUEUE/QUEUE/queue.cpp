387852a91c3bc06670a5a87b298886832a29e280
ANONYMOUS HOLY <deilttantest@gmail.com>
2017-12-08 20:58:44 +0900

    QUEUE

--- DataStructures/QUEUE/QUEUE/queue.cpp
+++ DataStructures/QUEUE/QUEUE/queue.cpp
@@ -1,82 +1,102 @@
-// https://www.acmicpc.net/problem/10845
 #define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <cstring>
+#include <stdio.h>
 
-#define MAX_N 100010
+#define MAX_N 110
+#define MAX_M 110
+int N, M;
+int maze[MAX_N][MAX_M];
+int dy[4] = { -1, +0, +1, +0 };
+int dx[4] = { +0, +1, +0, -1 };
+
+struct Node {
+public:
+	Node() {}
+	Node(int _y, int _x, int _cost) : y(_y), x(_x), cost(_cost) {}
+	Node(const Node &node) : y(node.y), x(node.x), cost(node.cost) {}
+	~Node() {}
+public:
+	int y, x;
+	int cost;
+};
+
 struct Queue {
 public:
-	Queue() : capacity(MAX_N) { 
-		begin = end = 0;
-		data = new int[capacity]; // 큐가 다 차지 않도록, 반드시 사이즈를 예상 입력 크기보다 더 크게 잡아준다
+	Queue() : capacity(10010), begin(0), end(0) { data = new Node[capacity]; }
+	~Queue() {}
+	void push_back(Node val) {
+		data[end++] = val;
+		if (end == capacity) end = 0;
 	}
-	~Queue() { if (data) delete[] data; }
-
-	void push_back(int _val) {
-		data[end] = _val;
-		if (++end == capacity) end = 0;
+	void pop_back() {
+		begin++;
+		if (begin == capacity) begin = 0;
 	}
-	void pop_front() {
-		if (isEmpty()) return;
-		if (++begin == capacity) begin = 0;
-	}
-	int front() {
-		if (isEmpty()) return -1;
-		return data[begin];
-	}
-	int back() {
-		if (isEmpty()) return -1;
-		int last = end - 1;
-		if (last == -1) last = capacity - 1;
-		return data[last];
-	}
-	int size() {
-		if (isEmpty()) return 0;
-		int length = 0, iter = begin;
-		while (iter != end) {
-			length++; iter++;
-			if (iter == capacity) iter = 0;
-		}
-		return length;
-	}
+	void init() { begin = end = 0; }
+	Node& front() { return data[begin]; }
 	bool isEmpty() { return begin == end; }
-private:
+public:
 	int begin, end;
 	int capacity;
-	int *data;
+	Node *data;
 };
+Queue queue;
+
+bool canMove(int y, int x) {
+	if (y < 1 || y > N) return false;
+	if (x < 1 || x > M) return false;
+	if (maze[y][x] == 0) return false;
+	return true;
+}
+
+int dist[MAX_N][MAX_M];
+bool discovered[MAX_N][MAX_M];
+int bfs(int srcY, int srcX, int desY, int desX) {
+	for (int y = 1; y <= N; y++) {
+		for (int x = 1; x <= N; x++) {
+			discovered[y][x] = false;
+			dist[y][x] = 0;
+		}
+	}
+	discovered[srcY][srcX] = true;
+
+	queue.init();
+	queue.push_back(Node(srcY, srcX, 1));
+	while (!queue.isEmpty()) {
+		int nodeY = queue.front().y;
+		int nodeX = queue.front().x;
+		int nodeCost = queue.front().cost;
+		queue.pop_back();
+
+		for (int direct = 0; direct < 4; direct++) {
+			int nextY = nodeY + dy[direct];
+			int nextX = nodeX + dx[direct];
+			int nextCost = nodeCost + 1;
+			if (canMove(nextY, nextX) && !discovered[nextY][nextX]) {
+				discovered[nextY][nextX] = true;
+				dist[nextY][nextX] = nextCost;
+				queue.push_back(Node(nextY, nextX, nextCost));
+			}
+		}
+	}
+	return dist[desY][desX];
+}
+
+void initMap(int row, char *input) {
+	for (int col = 1; col <= M; col++)
+		maze[row][col] = input[col - 1] - '0';
+}
 
 int main(int argc, char *argv[]) {
 	//freopen("sample_input.txt", "r", stdin);
-	setbuf(stdout, NULL);
+	//setbuf(stdout, NULL);
 
-	Queue queue;
-
-	int N; scanf("%d", &N);
-	for (int i = 1; i <= N; i++) {
-		char cmd[100]; scanf("%s", cmd);
-		if (strcmp(cmd, "push") == 0) {
-			int val; scanf("%d", &val);
-			queue.push_back(val);
-		}
-		else if (strcmp(cmd, "pop") == 0) {
-			printf("%d\n", queue.front());
-			queue.pop_front();
-		}
-		else if (strcmp(cmd, "front") == 0) {
-			printf("%d\n", queue.front());
-		}
-		else if (strcmp(cmd, "back") == 0) {
-			printf("%d\n", queue.back());
-		}
-		else if (strcmp(cmd, "size") == 0) {
-			printf("%d\n", queue.size());
-		}
-		else if (strcmp(cmd, "empty") == 0) {
-			if (queue.isEmpty()) printf("%d\n", 1);
-			else printf("%d\n", 0);
-		}
-		else printf("INVALID COMMAND: %s\n", cmd);
+	char input[MAX_M]; scanf("%d %d", &N, &M);
+	for (int row = 1; row <= N; row++) {
+		scanf("%s", input);
+		initMap(row, input);
 	}
+	int minDist = bfs(1, 1, N, M);
+	printf("%d\n", minDist);
+
 	return 0;
 }
