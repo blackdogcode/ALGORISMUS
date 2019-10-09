5d55d70dc4a2ef24b881cfb309137b00e3b36712
blackdogcode <blackdogcode@gmail.com>
2019-10-09 11:55:43 +0900

    DFS PackMan

---
+++ 28_DFS/DFS/DFS/dfs_pacman.cpp
@@ -0,0 +1,206 @@
+//
+//  AI: DFS(Depth Frist Search)
+//  https://www.hackerrank.com/challenges/pacman-dfs
+//  Created by Sangchul, Song on 3/24/16.
+//  Copyright © 2016 HYU. All rights reserved.
+//
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+enum NodeType { WALL, SPACE, PACMAN, FOOD };
+
+class Node
+{
+private:
+    // Member Variables
+    int x;
+    int y;
+    bool explored;
+    NodeType type;
+    Node* predecessor;
+public:
+    // Member Functions
+    int getX() { return x; }
+    int getY() { return y; }
+    Node* getPredecessor() { return predecessor; }
+    void setPredecessor(Node* newPredecessor) { predecessor = newPredecessor; }
+    void setExplored() { explored = true; }
+    bool isNotExplored() { return explored == false; }
+    bool isNotWall() { return type != WALL; }
+    bool isFood() { return type == FOOD; }
+    bool isPacman() { return type == PACMAN; }
+    void printPosition() { cout << x << " " << y << endl; }
+    
+    // Constructor & Destructor
+    Node() { ; } // Do nothing
+    Node(int newX, int newY, char shape);
+    ~Node();
+};
+
+void DFS(Node **maze, int packman_x, int packman_y, int food_x, int food_y, int row, int col);
+void printPath(Node* src, Node* des);
+
+int main(int argc, const char* argv[])
+{
+    // Initialization
+    int pacman_x, pacman_y;
+    int food_x, food_y;
+    int row, col;
+    cin >> pacman_x >> pacman_y;
+    cin >> food_x >> food_y;
+    cin >> row >> col;
+    
+    // Configuration Maze[row x col]
+    char shape;
+    Node** maze = new Node* [row];
+    for (int i = 0; i < row; i++)
+    {
+        maze[i] = new Node[col];
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> shape; // Wall(%), Space(-), Pacman(P), Food(.)
+            maze[i][j] = Node(i, j, shape);
+        }
+    }
+    
+    // Depth First Search: from 'pacman' to 'food'
+    DFS(maze, pacman_x, pacman_y, food_x, food_y, row, col);
+    
+
+    return 0;
+}
+
+inline void
+DFS (Node **maze, int pacman_x, int pacman_y, int food_x, int food_y, int row , int col)
+{
+    Node* pacman = &maze[pacman_x][pacman_y];
+    Node* food = &maze[food_x][food_y];
+    stack<Node*> dfsStack; // FIFO: Explored Nodes (Discovered but, not yet Conquered)
+    queue<Node*> dfsQueue; // FIFO: Expanded Nodes (Discovered and, also Conquered)
+    
+    pacman -> setExplored();
+    dfsStack.push(pacman);
+    while (!dfsStack.empty())
+    {
+        Node *node = dfsStack.top();
+        dfsStack.pop();
+        dfsQueue.push(node);
+        if (node -> isFood()) // Success Condition: Food Expanded
+            break;
+        else
+        {
+            int x = node -> getX();
+            int y = node -> getY();
+            Node *upNode, *leftNode, *rightNode, *downNode;
+            upNode = leftNode = rightNode = downNode = NULL;
+            
+            // Up node
+            if (x - 1 >= 0)
+                upNode = &maze[x - 1][y];
+            // Left node
+            if (y - 1 >= 0)
+                leftNode = &maze[x][y - 1];
+            // Right node
+            if (y + 1 < col)
+                rightNode = &maze[x][y + 1];
+            // Down node
+            if (x + 1 < row)
+                downNode = &maze[x + 1][y];
+            
+            if (upNode -> isNotExplored() && upNode -> isNotWall())
+            {
+                upNode -> setPredecessor(node);
+                upNode -> setExplored();
+                dfsStack.push(upNode);
+            }
+            if (leftNode -> isNotExplored() && leftNode -> isNotWall())
+            {
+                leftNode -> setPredecessor(node);
+                leftNode -> setExplored();
+                dfsStack.push(leftNode);
+            }
+            if (rightNode -> isNotExplored() && rightNode -> isNotWall())
+            {
+                rightNode -> setPredecessor(node);
+                rightNode -> setExplored();
+                dfsStack.push(rightNode);
+            }
+            if (downNode -> isNotExplored() && downNode -> isNotWall())
+            {
+                downNode -> setPredecessor(node);
+                downNode -> setExplored();
+                dfsStack.push(downNode);
+            }
+        }
+    }
+    // Print explored nodes during executing dfs
+    Node *exploredNode;
+    cout << dfsQueue.size() << endl; // # of expanded nodes
+    while (!dfsQueue.empty())
+    {
+        exploredNode = dfsQueue.front();
+        exploredNode -> printPosition();
+        dfsQueue.pop();
+    }
+    
+    // Pinrt path from packman to food
+    printPath(pacman, food);
+}
+
+// Print DFS path(not gurantee optimal) from src(packman) to des(food)
+void
+printPath(Node* src, Node* des)
+{
+    static int distance = 0;
+    if (des -> isPacman())
+    {
+        cout << distance << endl;
+        des -> printPosition();
+        return ;
+    }
+    else
+    {
+        distance++;
+        printPath(src, des -> getPredecessor());
+        des -> printPosition();
+    }
+}
+
+// Constructors
+Node:: Node(int newX, int newY, char shape)
+{
+    x = newX;
+    y = newY;
+    explored = false;
+    predecessor = NULL;
+    switch (shape)
+    {
+        case '%':
+            type = WALL;
+            break;
+        case '-':
+            type = SPACE;
+            break;
+        case 'P':
+            type = PACMAN;
+            break;
+        case '.':
+            type = FOOD;
+            break;
+        default:
+            break;
+    }
+}
+
+// Destructor
+Node::~Node()
+{
+    delete (predecessor);
+}
+
