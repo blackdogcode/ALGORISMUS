185f307d253af1be9282d6ae056c601f7a0a62ba
blackdogcode <blackdogcode@gmail.com>
2019-12-20 02:28:50 +0900

    Dijkstra Algorithm

--- 30_Shortest_Path_Problem/Dijesktra/Dijesktra.cpp
+++ 30_Shortest_Path_Problem/Dijesktra/Dijesktra.cpp
@@ -1,24 +1,50 @@
-﻿#include <iostream>
+﻿// https://www.coursera.org/learn/algorithms-graphs-data-structures/exam/Ij5au/programming-assignment-2/attempt
+// https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include <queue>
 
-#define MAX_V 100
-#define INF 123456789
-
 using namespace std;
 
-int V; // 정점 개수
+const int CNT_V = 201;
+const int INF = 1000000;
+vector<int> ret { 7, 37, 59, 82, 99, 115, 133, 165, 188, 197 };
 
-vector<pair<int, int>> adj[MAX_V];
+
+vector<pair<int, int>> edge[CNT_V];
 vector<int> dijkstra(int src);
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+	ifstream in("dijkstraData.txt", ifstream::in);
+	if (!in.is_open()) {
+		cerr << "Error: dijkstraData.txt not found!";
+		return -1;
+	}
+
+	while (!in.eof()) {
+		string line;
+		getline(in, line);
+		stringstream strStream(line);
+		int v; strStream >> v;
+		while (!strStream.eof()) {
+			int w, cost; strStream >> w >> cost;
+			edge[v].push_back(make_pair(w, cost));
+		}
+	}
+	// Exact Output: 2599,2610,2947,2052,2367,2399,2029,2442,2505,3068
+	vector<int> dist = dijkstra(1);
+	for (int i = 0; i < ret.size(); ++i) {
+		cout << dist[ret[i]] << ",";
+	}
+	return 0;
 }
 
 vector<int> dijkstra(int src) {
-	vector<int> dist(V, INF);
+	vector<int> dist(CNT_V, INF);
 	dist[src] = 0;
 	priority_queue<pair<int, int>> pq;
 	pq.push(make_pair(0, src));
@@ -29,9 +55,9 @@ vector<int> dijkstra(int src) {
 
 		if (dist[here] < cost) continue;
 
-		for (int i = 0; i < adj[here].size(); ++i) {
-			int there = adj[here][i].first;
-			int nextDist = cost + adj[here][i].second;
+		for (int i = 0; i < edge[here].size(); ++i) {
+			int there = edge[here][i].first;
+			int nextDist = cost + edge[here][i].second;
 
 			if (dist[there] > nextDist) {
 				dist[there] = nextDist;
