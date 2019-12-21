0768da70d20b742585cef1ed4bb1f1aad864b20e
blackdogcode <blackdogcode@gmail.com>
2019-12-22 00:31:48 +0900

    Karger Min Cut Algorithm

---
+++ Stanford_Algorithm_Assignment/1_Divide_and_Conquer_Sorting_and_Searching_and_Randomized_Algorithms/Week4_kargerMinCut/kargerMinCut/kargerMinCut.cpp
@@ -0,0 +1,36 @@
+// kargerMinCut Algorithm
+// https://en.wikipedia.org/wiki/Karger%27s_algorithm
+// https://www.geeksforgeeks.org/kargers-algorithm-for-minimum-cut-set-1-introduction-and-implementation/
+// https://www.geeksforgeeks.org/kargers-algorithm-for-minimum-cut-set-2-analysis-and-applications/
+
+// The file contains the adjacency list representation of a simple undirected graph.
+// There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, 
+// and the particular row(other entries except the first column) tells all the vertices that the vertex 
+// is adjacent to.So for example, the 6th row looks like : "6	155	56	52	120	......".
+// This just means that the vertex with label 6 is adjacent to(i.e., shares an edge with) 
+// the vertices with labels 155, 56, 52, 120, ......, etc
+//
+// Your task is to code upand run the randomized contraction algorithm for the min cut problemand 
+// use it on the above graph to compute the min cut. 
+// (HINT : Note that you'll have to figure out an implementation of edge contractions. 
+// Initially, you might want to do this naively, creating a new graph from the old every time 
+// there's an edge contraction.But you should also think about more efficient implementations.) 
+// (WARNING : As per the video lectures, please make sure to run the algorithm many times with different random seeds, 
+// and remember the smallest cut that you ever find.) Write your numeric answer in the space provided.So e.g., 
+// if your answer is 5, just type 5 in the space provided.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+   cin.sync_with_stdio(false);
+   ifstream in("kargerMinCut.txt", ifstream::in);
+   if (!in.is_open()) {
+      cerr << "Erorr: kargerMinCut.txt Not Found!";
+      return -1;
+   }
+   // Correct Output: 17
+   return 0;
+}
