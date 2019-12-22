3df507d0016731f49f22d34bd91b370646a0daea
blackdogcode <blackdogcode@gmail.com>
2019-12-22 14:02:06 +0900

    deleted

--- Stanford_Algorithm_Assignment/2_Graph_Search_Shortest_Paths_and_Data_Structures/Week4_2SUM/2SUM/2SUM.cpp
+++ Stanford_Algorithm_Assignment/2_Graph_Search_Shortest_Paths_and_Data_Structures/Week4_2SUM/2SUM/2SUM.cpp
@@ -1,20 +1,18 @@
-// 2SUM.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 2SUM.cpp : 
 //
 
 #include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+	ifstream in("2sum.txt", ifstream::in);
+	if (!in.is_open()) {
+		cerr << "Error: 2sum.txt not found!" << endl;
+		return -1;
+	}
+	// Exact Output: 427
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
