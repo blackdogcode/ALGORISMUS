30453a7b1285f86eadd61b8e15bb1cfcc2206e63
blackdogcode <blackdogcode@gmail.com>
2020-06-29 18:31:37 +0900

    alter print format

--- Others/PROJECT_1/PROJECT_1.c
+++ Others/PROJECT_1/PROJECT_1.c
@@ -133,7 +133,7 @@ void print_result(int* my_lotto, int* lotto, int result, FILE* fid) {
 	for (int i = 0; i < CNT_LOTTO; ++i) fprintf(fid, "%d ", my_lotto[i]);
 	fprintf(fid, "\n");
 	fprintf(fid, "    당첨번호 갯수  : %d\n", chk);
-	fprintf(fid, "    당첨된 번호    : ");
+	fprintf(fid, "    당첨된 번호     : ");
 	if (chk == 0) { fprintf(fid, "없음"); }
 	else {
 		for (int i = 0; i < CNT_LOTTO; ++i) {
